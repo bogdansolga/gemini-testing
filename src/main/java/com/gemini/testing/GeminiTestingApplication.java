b5b48677a5588c8cd51536fa200e1a22b9a95fa4
Bogdan Solga <bogdan.solga@gmail.com>
2024-09-21 18:39:29 +0300

    [improve] Reuse the code from https://docs.spring.io/spring-ai/reference/api/chat/vertexai-gemini-chat.html

--- src/main/java/com/gemini/testing/GeminiTestingApplication.java
+++ src/main/java/com/gemini/testing/GeminiTestingApplication.java
@@ -1,8 +1,11 @@
 package com.gemini.testing;
 
-import org.springframework.ai.chat.client.ChatClient;
+import com.google.cloud.vertexai.VertexAI;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,19 +18,28 @@ public class GeminiTestingApplication {
 		SpringApplication.run(GeminiTestingApplication.class, args);
 	}
 
+	@Value("${spring.ai.vertex.ai.gemini.projectId}")
+	private String projectId;
+
+	@Value("${spring.ai.vertex.ai.gemini.location}")
+	private String location;
+
 	@Bean
-	public ApplicationRunner runner(ChatClient client) {
+	public ApplicationRunner runner() {
 		return args -> {
-			ChatClient.CallPromptResponseSpec responseSpec = client.prompt(new Prompt(
-																		   "Why is Gemini so difficult to configure?", getChatOptions()))
-																   .call();
-			System.out.println(responseSpec.content());
+			VertexAI vertexApi =  new VertexAI(projectId, location);
+			var chatModel = new VertexAiGeminiChatModel(vertexApi, getChatOptions());
+			ChatResponse response = chatModel.call(new Prompt("Why is Gemini so difficult to configure?"));
+			System.out.println(response.getResult()
+									   .getOutput()
+									   .getContent());
 		};
 	}
 
 	private VertexAiGeminiChatOptions getChatOptions() {
 		return VertexAiGeminiChatOptions.builder()
 										.withTemperature(0.4f)
+										.withModel(VertexAiGeminiChatModel.ChatModel.GEMINI_1_5_PRO)
 										.build();
 	}
 }
